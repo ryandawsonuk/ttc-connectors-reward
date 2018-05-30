3219bd71918aeb89c3c4c7e0714b590a63fe99c6
Salaboy <salaboy@gmail.com>
2018-05-30 10:54:31 +0100

    adding welcome endpoint

--- src/main/java/org/activiti/cloud/connectors/reward/RewardCloudConnectorApp.java
+++ src/main/java/org/activiti/cloud/connectors/reward/RewardCloudConnectorApp.java
@@ -6,6 +6,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @SpringBootApplication
 @EnableActivitiCloudConnector
@@ -17,6 +19,11 @@ public class RewardCloudConnectorApp implements CommandLineRunner {
     public static void main(String[] args) {
         SpringApplication.run(RewardCloudConnectorApp.class,
                               args);
+    }
+
+    @RequestMapping(method = RequestMethod.GET, path = "/")
+    public String welcome() {
+        return "Welcome to the Campaign Engagement Rewards Service";
     }
 
     @Override
